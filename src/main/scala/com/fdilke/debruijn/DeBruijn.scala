319557dc5df1f2bdbda7751ee372f1c239057fc8
Felix Dilke <fdilkeuk@yahoo.co.uk>
2024-03-24 10:17:00 +0000

    fixed the De Bruin algo. Stil imperative, opaque

--- src/main/scala/com/fdilke/debruijn/DeBruijn.scala
+++ src/main/scala/com/fdilke/debruijn/DeBruijn.scala
@@ -1,87 +1,44 @@
 package com.fdilke.debruijn
 
-import scala.collection.mutable
-import java.util.Vector
 
 // Calculate a De Bruijn sequence ( https://en.wikipedia.org/wiki/De_Bruijn_sequence )
 // Return a cyclic sequence of length n^k in which every n-sequence from the k-alphabet occurs.
 object DeBruijn:
+  
+    // original Python algo to generate 'sequence' (from Wikipedia page above)
+    // a = [0] * k * n
+    // sequence = []
+    // def db(t, p):
+    //     if t > n:
+    //         if n % p == 0:
+    //             sequence.extend(a[1 : p + 1])
+    //     else:
+    //         a[t] = a[t - p]
+    //         db(t + 1, p)
+    //         for j in range(a[t - p] + 1, k):
+    //             a[t] = j
+    //             db(t + 1, t)
+
+    // db(1, 1)
+
+  def string(n : Int, k: Int): String =
+    apply(n, k).map { i => ('0' + i).toChar }.mkString
+
   def apply(n: Int, k: Int): Seq[Int] =
-    val alphabet = (0 until k).map { i => ('0' + i).toChar }.mkString
-    val string = deBruijn(n, k, alphabet)
-    string.toCharArray.toSeq.map { c => c - '0' }
-
-  private val seen: mutable.Set[String] = new mutable.HashSet[String]()
-  private val edges: Vector[Integer] = new Vector[Integer]()
- 
-  // Function to find a de Bruijn sequence
-  // of order n on k characters
-  def deBruijn(n : Int, k: Int, a: String): String =
-      // Clearing global variables
-      seen.clear()
-      edges.clear()
-
-      val startingNode: String = string(n - 1, a.charAt(0))
-      dfs(startingNode, k, a)
-
-      var s: String = ""
-
-      // Number of edges
-      val l: Int = Math.pow(k, n).toInt
-      for { i <- 0 until l }
-          s = s + a.charAt(edges.get(i))
-      s += startingNode
-      s
-
-    // Modified DFS in which no edge
-    // is traversed twice
-  def dfs(node: String, k: Int, a: String): Unit =
-      for { i <- 0 until k }
-          val str: String = node + a.charAt(i)
-          if (!seen.contains(str)) then
-              seen.add(str)
-              dfs(str.substring(1), k, a)
-              edges.add(i)
-
-  def string(n : Int, charAt: Char): String =
-      var str: String = ""
-      for { i <- 0 until n } 
-          str = str + charAt
-      str
-
-
-  def badApply(n: Int, k: Int): Seq[Int] =
-    val a: Array[Int] = Array.ofDim(n * k + 1)
-    println("a = " + a.toSeq)
-    var seq: Seq[Int] = Seq.empty
-    def db(t: Int, p : Int): Unit =
-      if (t > n) {
-        if (n % p == 0) {
-          println("doing thing")
-          seq ++= a.toSeq.slice(1, p + 1)
-        }
-      } else {
-            a(t) = a(t - p)
-            db(t + 1, p)
-            for (j <- a(t - p) + 1 to k) {
-                a(t) = j
-                db(t + 1, t)    
-            }
-      }
-    db(1, 1)
-    println("seq = " + seq)
-    // seq
-    Seq.empty
-
-    
-  // def apply(n: Int, k: Int): Seq[Int] =
-  //   apply(n, 0 until k)
-
-  // def apply[T](
-  //   n: Int,
-  //   alphabet: Seq[T]
-  // ): Seq[T] =
-  //   val k = alphabet.length
-  //   val a: Array[Int] =
-  //   Seq.empty
-
+    if k == 1 then
+      Seq(0)
+    else
+      val a: Array[Int] = new Array(n * k)
+      var sequence: Seq[Int] = Seq.empty
+      def db(t: Int, p: Int): Unit =
+        if t > n then
+          if (n % p == 0) then
+            sequence ++= (1 to p) map a
+        else
+          a(t) = a(t - p)
+          db(t + 1, p)
+          for { j <- (a(t - p) + 1) until k }
+            a(t) = j
+            db(t + 1, t)
+      db(1, 1)
+      sequence
--- src/test/scala/com/fdilke/debruijn/DeBruijnSpec.scala
+++ src/test/scala/com/fdilke/debruijn/DeBruijnSpec.scala
@@ -4,19 +4,16 @@ import munit.FunSuite
 import com.fdilke.utility.RichFunSuite._
 
 class DeBruijnSpec extends FunSuite:
-  // test("calculate trivial case (n=1, k=1)"):
-  //   DeBruijn(n=1, k=1) is Seq.empty
-  test("dummy test for output"):
-    for { p <- 1 until 7 }
-      println(s"p=$p => ${ DeBruijn(n=p, k=2).mkString }")
-    println(s"math thing 2^3 = ${ Math.pow(2, 3).toInt }")
-    println(s"GDG(3, 2, xx) = ${ GFG.deBruijn(3, 2, "oi") }")
-    1 is 1
+  test("calculate trivial case (n=1, k=1)"):
+    DeBruijn.string(n=1, k=1) is "0"
+  test("calculate almost-trivial case (n=1, k=2)"):
+    DeBruijn.string(n=1, k=2) is "01"
   test("calculate less trivial case"):
-    DeBruijn(n=1, k=2) is Seq(1, 0)
-  test("calculate case n = 2"):
-    DeBruijn(n=2, k=2) is Seq(0, 1, 1, 0, 0)
-  test("calculate case n = 3"):
-    DeBruijn(n=3, k=2) is Seq(0, 0, 1, 1, 1, 0, 1, 0, 0, 0)
+    DeBruijn.string(n=1, k=2) is "01"
+  test("calculate case n = 2, k = 2"):
+    DeBruijn.string(n=2, k=2) is "0011"
+  test("calculate case n = 3, k = 2"):
+    DeBruijn.string(n=3, k=2) is "00010111" 
+  test("calculate case n = 2, k = 3"):
+    DeBruijn.string(n=2, k=3) is "001021122" 
     
-    // 0011101000
