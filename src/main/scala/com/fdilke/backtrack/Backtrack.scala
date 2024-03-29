df1745edd67e5963fae8a9052817c635c64c9926
Felix Dilke <fdilkeuk@yahoo.co.uk>
2024-03-29 22:09:48 +0000

    tidying up

--- src/main/scala/com/fdilke/backtrack/Backtrack.scala
+++ src/main/scala/com/fdilke/backtrack/Backtrack.scala
@@ -2,7 +2,7 @@ package com.fdilke.backtrack
 
 import scala.collection.immutable.HashMap
 
-object Backtrack {
+object Backtrack:
   trait DecisionNode[KEY, VALUE] extends Function[
     Map[KEY, VALUE],
     NextStep[KEY, VALUE]
@@ -23,7 +23,6 @@ object Backtrack {
       )
   
   sealed trait NextStep[KEY, VALUE]
-
   case object MapComplete extends NextStep[_, _]
   case object MapInvalid extends NextStep[_, _]
   case class MapContinue[KEY, VALUE](
@@ -34,15 +33,15 @@ object Backtrack {
   def solve[KEY, VALUE](
      values: Iterable[VALUE],
      initialNode: DecisionNode[KEY, VALUE]
-  ): Iterable[Map[KEY, VALUE]] = {
+  ): Iterable[Map[KEY, VALUE]] =
     def recurse(
       partialMap: Map[KEY, VALUE],
       step: NextStep[KEY, VALUE]
     ): Iterator[Map[KEY, VALUE]] =
-      step match {
+      step match
         case MapInvalid =>
           Iterator.empty
-        case MapComplete /* .asInstanceOf[NextStep[KEY, VALUE]] */ =>
+        case MapComplete =>
           Iterator(partialMap)
         case MapContinue(key, node) =>
           for {
@@ -51,12 +50,8 @@ object Backtrack {
             solution <- recurse(newMap, node(newMap))
           } yield
             solution
-      }
 
     val initialMap: Map[KEY, VALUE] = new HashMap[KEY, VALUE]()
-    new Iterable[Map[KEY, VALUE]] {
+    new Iterable[Map[KEY, VALUE]]:
       override def iterator: Iterator[Map[KEY, VALUE]] =
         recurse(initialMap, initialNode(initialMap))
-    }
-  }
-}
