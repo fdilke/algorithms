181a2930e3d9be1bb7d8f38375e5153cc3530a84
Felix Dilke <fdilkeuk@yahoo.co.uk>
2024-03-30 11:41:39 +0000

    basic recursive backtracking

--- src/main/scala/com/fdilke/backtrack/node/Node.scala
+++ src/main/scala/com/fdilke/backtrack/node/Node.scala
@@ -8,10 +8,15 @@ trait Node[+NODE <: Node[NODE, SOLUTION], +SOLUTION]:
     explore match
       case NodeBad => None
       case NodeGood(solution) => Some(solution)
+      case NodeContinue[NODE, SOLUTION](nextNodes) => 
+        nextNodes.map { _.solve  }.find { _.isDefined }.flatten
 
 sealed trait NodeStatus[+NODE <: Node[NODE, SOLUTION], +SOLUTION]
 
 case class NodeGood[+SOLUTION](solution: SOLUTION) extends NodeStatus[Nothing, SOLUTION]
 case object NodeBad extends NodeStatus[Nothing, Nothing]
+case class NodeContinue[+NODE <: Node[NODE, SOLUTION], +SOLUTION](
+  nextNodes: Iterable[NODE]
+) extends NodeStatus[NODE, SOLUTION]
 
 
--- src/test/scala/com/fdilke/backtrack/node/NodeSpec.scala
+++ src/test/scala/com/fdilke/backtrack/node/NodeSpec.scala
@@ -19,3 +19,13 @@ class NodeSpec extends FunSuite:
     val node = QuickWinNode()
     node.solve is Some(2)
 
+  test("successfully increments a value to 5"):
+    class SearchNode(i: Int) extends Node[SearchNode, Boolean]:
+      override def explore: NodeStatus[SearchNode, Boolean] =
+        if (i == 5)
+          NodeGood(true)
+        else
+          NodeContinue(Iterable(SearchNode(i + 1)))
+    val initialNode = SearchNode(0)
+    initialNode.solve is Some(true)
+
