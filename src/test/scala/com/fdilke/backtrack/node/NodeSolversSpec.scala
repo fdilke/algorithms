94cd8521a78c8e1165ced5fe50c99019d91d90b2
fdilke <fdilke@gmail.com>
2024-04-21 00:40:01 +0100

    HackStack shows the way

--- src/test/scala/com/fdilke/backtrack/node/NodeSolversSpec.scala
+++ src/test/scala/com/fdilke/backtrack/node/NodeSolversSpec.scala
@@ -87,3 +87,19 @@ abstract class NodeSolverSpec(solver: NodeSolver) extends FunSuite:
 //        Seq(true)
 //      )
 //    println("Error = " + error)
+
+object HackStack extends App:
+  def corecurse(depth: Int) =
+    recurse(depth)
+  def recurse(depth: Int): Unit =
+    if depth == 40 then
+      println("Hit max depth")
+      val trace: Seq[StackTraceElement] =
+        Thread.currentThread().getStackTrace.toSeq
+      println("size = " + trace.size)
+      trace.foreach { tel =>
+        println("tel = " + tel.toString)
+      }
+    else
+      corecurse(depth + 1)
+  recurse(0)
