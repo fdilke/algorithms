d579b0445b0e0ddfdd3ec72e41fd5a7fd2cb1b84
fdilke <fdilke@gmail.com>
2024-12-17 11:17:35 +0000

    minor tidyings

--- src/test/scala/com/fdilke/truchet/TruchetApp.scala
+++ src/test/scala/com/fdilke/truchet/TruchetApp.scala
@@ -4,26 +4,26 @@ import java.awt._
 import javax.swing.{JFrame, JPanel, WindowConstants}
 
 object TruchetApp extends App:
-  val device: GraphicsDevice = 
+  val device: GraphicsDevice =
     GraphicsEnvironment.getLocalGraphicsEnvironment.getDefaultScreenDevice
 
-  val test = new TruchetFrame(device)
-  test.initComponents()
-  test.begin()
+  new TruchetFrame(device)
 
 class TruchetFrame(
   device: GraphicsDevice
 ) extends JFrame("Truchet Frame", device.getDefaultConfiguration):
 
   setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE)
+  initComponents()
+  begin()
 
-  def initComponents(): Unit =
+  private def initComponents(): Unit =
     val contentPane = getContentPane
     contentPane.setLayout(new BorderLayout)
-    val modesPanel = new TruchetPanel
-    contentPane.add(modesPanel, BorderLayout.CENTER)
+    val panel = new TruchetPanel
+    contentPane.add(panel, BorderLayout.CENTER)
 
-  def begin(): Unit =
+  private def begin(): Unit =
     val isFullScreen: Boolean = device.isFullScreenSupported
     setUndecorated(isFullScreen)
     setResizable(!isFullScreen)
@@ -39,3 +39,5 @@ class TruchetPanel extends JPanel(new GridLayout(1, 2)):
     val dimension = getSize()
     graphics.setColor(Color.BLACK)
     graphics.drawLine(0, 0, dimension.width, dimension.height)
+    graphics.drawLine(0, 0, dimension.width/2, dimension.height)
+    graphics.drawLine(0, 0, dimension.width, dimension.height/2)
