4e80c31a81e1115203739f091062a37349e4c2ca
fdilke <fdilke@gmail.com>
2025-01-03 21:07:57 +0000

    more tidying

--- src/test/scala/com/fdilke/utility/RichFunSuite.scala
+++ src/test/scala/com/fdilke/utility/RichFunSuite.scala
@@ -2,8 +2,6 @@ package com.fdilke.utility
 
 import munit.{ FunSuite, Assertions }
 import Assertions.{ assertEquals, assertNotEquals }
-// import com.fdilke.bewl2.sets.SetsWithSlowActions
-// import com.fdilke.utility.Shortcuts._
 
 object RichFunSuite:
   extension[A] (a: A)
@@ -16,20 +14,9 @@ object RichFunSuite:
     inline def be(text: String): Unit =
       assertEquals(letters.mkString, text)
 
-  // import SetsWithSlowActions._
-
-  // implicit class arrowComparisons[A: Dot, B: Dot](arrow: A => B):
-  //   inline def isArrow(arrow2: A => B): Unit =
-  //     assert(arrow =!= arrow2)
-  //   inline def isNotArrow(arrow2: A => B): Unit =
-  //     assert(!(arrow =!= arrow2))
-
   def checkSameElementsAs[X](thing: Seq[X], other: Seq[X]): Unit =
     thing.size is other.size
     thing.toSet is other.toSet
-  // extension(text: String)
-  //   inline def is(letters: Seq[Char]): Unit =
-  //     assertEquals(letters, text.toSeq)
 
 
 
