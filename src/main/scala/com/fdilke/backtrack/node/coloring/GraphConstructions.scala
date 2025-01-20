d248d581fc314e5f480641a8042d5847ada3be4f
fdilke <fdilke@gmail.com>
2025-01-20 13:33:38 +0000

    torus graph tests, embryonic odd graph

--- src/main/scala/com/fdilke/backtrack/node/coloring/GraphConstructions.scala
+++ src/main/scala/com/fdilke/backtrack/node/coloring/GraphConstructions.scala
@@ -20,3 +20,9 @@ object GraphConstructions:
             cellIndex(i, j) -> cellIndex(i, j + 1)
           )
       pairs.flatten.distinct
+
+//"The odd graph O_{n} has one vertex for each of the (n-1)-element subsets of a (2n-1)}-element set.
+//Two vertices are connected by an edge if and only if the corresponding subsets are disjoint."
+
+  def oddGraph(n: Int): Seq[(Int, Int)] =
+    Seq.empty
--- src/test/scala/com/fdilke/backtrack/node/coloring/GraphConstructionsSpec.scala
+++ src/test/scala/com/fdilke/backtrack/node/coloring/GraphConstructionsSpec.scala
@@ -1,23 +1,41 @@
 package com.fdilke.backtrack.node.coloring
 
-import com.fdilke.utility.RichFunSuite._
+import com.fdilke.utility.RichFunSuite.*
 import munit.FunSuite
+import GraphConstructions.*
 
-import GraphConstructions._
+import scala.Seq
 
 class GraphConstructionsSpec extends FunSuite:
   
   test("torus(0, 0) is empty"):
     torus(0, 0).isEmpty is true
-    
+
   test("torus(0, 3) is empty"):
     torus(0, 3).isEmpty is true
-    
+
   test("torus(3, 0) is empty"):
     torus(3, 0).isEmpty is true
-    
+
   test("torus(1, 1) has 1 vertex"):
     torus(1, 1) is Seq(0 -> 0)
+
+  test("torus(3, 2) has 6 vertices and 12 edges"):
+    val torus32: Seq[(Int, Int)] = torus(3, 2)
+    torus32.flatMap:
+      case (v: Int, w: Int) => Seq(v, w).toSet
+    .distinct.sorted is (0 until 6)
+    torus32.size is 12
+    torus32.sorted is Seq(
+      (0, 1), (0, 2),
+      (1, 0), (1, 3),
+      (2, 3), (2, 4),
+      (3, 2), (3, 5),
+      (4, 0), (4, 5),
+      (5, 1), (5, 4)
+    )
+
+  // todo: add more adequate torus tests... but this is better than nothing
 
 
 
