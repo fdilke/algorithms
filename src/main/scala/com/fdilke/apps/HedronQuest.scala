9db68fd7b0e5e073c0333c22c9feb99326ec1c7f
fdilke <fdilke@gmail.com>
2025-02-01 19:45:25 +0000

    verifying the geometric/combinatorial properties of the plushie as reconstructed from its group structure

--- src/main/scala/com/fdilke/apps/HedronQuest.scala
+++ src/main/scala/com/fdilke/apps/HedronQuest.scala
@@ -63,5 +63,75 @@ object PlushiePlayground extends App:
   given Group[Permutation] = group
   val f = Permutation(0, 2, 3, 4, 1)
   val v = Permutation(2, 4, 1, 0, 3)
-  println("Product order = " + (f * v).order)
-  println("Group order = " + group.generateSubgroup(f, v).order)
+  val e = f * v
+  assert(e.order == 2)
+  val plushieGroup: group.Subgroup =
+    group.generateSubgroup(f, v)
+  println(s"Plushie group order = ${plushieGroup.order}")
+  type Coset = Set[Permutation]
+  type Corner= (Coset, Coset)
+  def multiplyCoset(
+     coset: Coset,
+     g: Permutation
+   ): Coset =
+    coset.map:
+      s => s * g
+  def rightCosetsOf(subgroup: group.Subgroup): Seq[Coset] =
+    group.elements.map: g =>
+      multiplyCoset(subgroup.elements, g)
+    .toSeq
+  def incident(c1: Coset, c2: Coset): Boolean =
+    (c1 intersect c2).nonEmpty
+  val fGroup: group.Subgroup = group.generateSubgroup(f)
+  val vGroup: group.Subgroup = group.generateSubgroup(v)
+  val eGroup: group.Subgroup = group.generateSubgroup(e)
+  val vertexes = rightCosetsOf(fGroup)
+  val faces = rightCosetsOf(vGroup)
+  val edges = rightCosetsOf(eGroup)
+  println(s"${vertexes.size} vertexes")
+  println(s"${faces.size} faces")
+  println(s"${edges.size} edges")
+  val corners: Seq[Corner] =
+    for
+      face <- faces
+      vertex <- vertexes if incident(face, vertex)
+    yield
+      (face, vertex)
+  println(s"${corners.size} corners")
+  def multiplyCorner(
+    corner: Corner,
+    g: Permutation
+  ): Corner =
+    (
+      multiplyCoset(corner._1, g),
+      multiplyCoset(corner._2, g)
+    )
+  val sampleCorner: Corner = corners.head
+  val cornerCoset: Set[Corner] =
+    group.elements.map: g =>
+      multiplyCorner(sampleCorner, g)
+  if cornerCoset.size == group.order then
+    println("Group acts torsorially on corners")
+  else
+    println("Not torsorial :(")
+  def checkIncidences(
+    seq1: Seq[Coset],
+    seq2: Seq[Coset],
+    theCount: Int
+  ): Boolean =
+    seq1.forall: s1 =>
+      seq2.count: s2 =>
+        incident(s1, s2)
+      == theCount
+  if checkIncidences(faces, vertexes, 5) then
+    println("Every face is incident to 5 vertexes")
+  if checkIncidences(faces, edges, 5) then
+    println("Every face is incident to 5 edges")
+  if checkIncidences(vertexes, faces, 4) then
+    println("Every vertex is incident to 4 faces")
+  if checkIncidences(vertexes, edges, 4) then
+    println("Every vertex is incident to 4 edges")
+  if checkIncidences(edges, vertexes, 2) then
+    println("Every edge is incident to 2 vertexes")
+  if checkIncidences(vertexes, edges, 4) then
+    println("Every vertex is incident to 4 edges")
