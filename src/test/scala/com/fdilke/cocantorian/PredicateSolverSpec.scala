95aa44f106075c001910af560c764d702fa145ab
fdilke <fdilke@gmail.com>
2025-03-17 13:32:00 +0000

    testing the number of solution steps

--- src/test/scala/com/fdilke/cocantorian/PredicateSolverSpec.scala
+++ src/test/scala/com/fdilke/cocantorian/PredicateSolverSpec.scala
@@ -1,25 +1,45 @@
 package com.fdilke.cocantorian
 
 import munit.FunSuite
-import com.fdilke.utility.RichFunSuite._
-import com.fdilke.utility.SetsUtilities._
+import com.fdilke.utility.RichFunSuite.*
+import com.fdilke.utility.SetsUtilities.*
 
-class ExPredicateSolverSpec extends PredicateSolverSpec(ExPredicateSolver)
+import java.util.concurrent.atomic.AtomicInteger
+
+class ExPredicateSolverSpec extends PredicateSolverSpec(ExPredicateSolver):
+  test("find a simple predicate"):
+    solvesIn(7,
+      2 -> true,
+      3 -> false
+    ): f =>
+      f(2) & !f(3)
+
 
 class PredicateSolverSpec(
   solver: PredicateSolver
 )  extends FunSuite:
-  test("find a simple predicate"):
-    solver[Int]:
-      f => f(2) && !f(3)
-    is:
+  def solvesIn(
+    maxCalls: Int,
+    entries: (Int, Boolean)*
+  )(
+    f: (Int => Boolean) => Boolean
+  ): Unit =
+    val numCalls = AtomicInteger()
+    solver[Int]: int2bool =>
+      numCalls.incrementAndGet()
+      f(int2bool)
+    .is:
       Some:
-        Map[Int, Boolean](
-          2 -> true,
-          3 -> false
-        )
-    
-  test("detect an impossible predicate equation"):
+        entries.toMap
+    println(s"numCalls = $numCalls")
+    (numCalls.get <= maxCalls) is true
+
+  test("solve for a trivial predicate"):
+    solvesIn(1): f =>
+      true
+
+  test("detect an impossible equation"):
     solver[Int]: f=>
       f(2) == f(3) && f(3) == !f(4) && f(4) == f(2)
-    .is(None)
+    is:
+      None
