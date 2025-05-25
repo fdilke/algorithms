0e014924fb2ab9a0f22a4547e7384348301a932c
fdilke <fdilke@gmail.com>
2025-05-25 12:39:50 +0100

    flawed implementation of build-classes

--- src/main/racket/demo/utility.rkt
+++ src/main/racket/demo/utility.rkt
@@ -33,11 +33,30 @@
   (vector->list array)
 )
 
+(define (class-indices canonicals)
+  (define indices (make-hash))
+  (define index 0)
+  (for/list ([c canonicals])
+    (define lookup (hash-ref indices c null))
+    (cond
+      ((null? lookup) (hash-set! indices c index) (set! index (add1 index)) (sub1 index))
+      (else c)
+    )
+  )
+)
+
+(define (build-classes size relators)
+  (class-indices
+    (build-equivalence size relators)
+  )
+)
+
 (provide
  (contract-out
   ;; iterate a function on a start value until it's fixed
     [iterate-to-fixed (parametric->/c [A] (-> A (-> A A) A))]
     [build-equivalence (parametric->/c [A] (-> number? (listof (cons/c A A)) (listof A)))]
+    [class-indices (parametric->/c [A] (-> (listof A) (listof number?)))]
   )
 )
   
@@ -61,7 +80,10 @@
   (check-equal? (build-equivalence 10 '((1 2)(7 0)(4 3) (3 7)(6 5)(9 5))) '(0 2 2 0 0 5 5 0 8 5))
   (check-equal? (build-equivalence 4 '((2 0)(3 1)(2 2)(3 3)(1 3)(0 2)(2 2)(3 3))) '(0 1 0 1))
   (check-equal? (build-equivalence 4 '((0 0)(0 1)(0 2)(0 3)(1 0)(1 1)(1 2)(1 3)(2 0)(2 1)(2 2)(2 3)(3 0)(3 1)(3 2)(3 3))) '(3 3 3 3))
-)
-
-
+  (check-equal? (class-indices '(1 0)) '(0 1))
+  (check-equal? (class-indices '(2 3 0)) '(0 1 2))
+;;  (check-equal? (class-indices '(2 2 0 0 2 2 1 1)) '(0 0 1 1 0 0 2 2))
+;;  (check-equal? (class-indices '(0 2 2 3)) '(0 1 1 2))
+;;  (check-equal? (build-classes 4 '((1 2))) '(0 1 1 2))
+) 
 
