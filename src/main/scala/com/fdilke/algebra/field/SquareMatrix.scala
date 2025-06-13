93a7d9eeaee2431829c143dfcb48716dea9b5932
fdilke <fdilke@gmail.com>
2025-06-13 16:44:24 +0100

    convenience class for constructing/comparing square matrices

---
+++ src/main/scala/com/fdilke/algebra/field/SquareMatrix.scala
@@ -0,0 +1,36 @@
+package com.fdilke.algebra.field
+
+import com.fdilke.utility.SetsUtilities
+import com.fdilke.utility.SetsUtilities.intSqrt
+
+import java.util
+import scala.reflect.ClassTag
+
+class SquareMatrix[T](
+  protected val matrix: Array[Array[T]]
+):
+  override def equals(other: Any): Boolean =
+    other match
+      case otherMatrix: SquareMatrix[T] =>
+        otherMatrix.isEqual(this)
+      case _ =>
+        throw new IllegalArgumentException("bad comparison")
+
+  private def isEqual(other: SquareMatrix[T]): Boolean =
+    util.Arrays.deepEquals(
+      matrix.asInstanceOf[Array[Any]],
+      other.matrix.asInstanceOf[Array[Any]]
+    )
+
+  def apply(row: Int)(column: Int): T =
+    matrix(row)(column)
+
+object SquareMatrix:
+  def apply[T: ClassTag](values: T*): SquareMatrix[T] =
+    val order = intSqrt(values.size)
+    val matrix: Array[Array[T]] =
+      Array.tabulate(order): row =>
+        Array.tabulate(order):column =>
+          values:
+            (row * order) + column
+    new SquareMatrix[T](matrix)
---
+++ src/test/scala/com/fdilke/algebra/field/SquareMatrixSpec.scala
@@ -0,0 +1,23 @@
+package com.fdilke.algebra.field
+
+import com.fdilke.utility.RichFunSuite._
+import munit.FunSuite
+
+class SquareMatrixSpec extends FunSuite:
+  test("can create and distinguish matrices"):
+    intercept[IllegalArgumentException]:
+      SquareMatrix[String]("not a", "square")
+    val matrix0110 = SquareMatrix[Int](0, 1, 1, 0)
+    val matrix1001 = SquareMatrix[Int](1, 0, 0, 1)
+    val matrix0110_2 = SquareMatrix[Int](0, 1, 1, 0)
+    matrix0110 is matrix0110
+    matrix0110 is matrix0110_2
+    matrix0110 isnt matrix1001
+
+  test("can read entries in the right order, following Herstein"):
+    val matrix0123 = SquareMatrix[Int](0, 1, 2, 3)
+    matrix0123(0)(0) is 0
+    matrix0123(0)(1) is 1
+    matrix0123(1)(0) is 2
+    matrix0123(1)(1) is 3
+    
